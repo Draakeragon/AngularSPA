f67b76752cecbc52543b2037482b2755fbfa6c49
Adriaan van Den Berg <adriaan.vandenberg@gmail.com>
2017-03-21 13:48:35 +0100

    Application works again! Testbuild

--- script/directive.js
+++ script/directive.js
@@ -1,5 +1,5 @@
 var app = angular.module("bookApp", []);
-app.directive("TestDirective", function() {
+app.directive("testDirective", function() {
     return {
         restrict : "A",
         template : "<h1>Made by a directive!</h1>"
