5830b76512ebc9b46c057de1034d9cfc3153bddd
Adriaan van Den Berg <adriaan.vandenberg@gmail.com>
2017-03-23 09:42:14 +0100

    Directive verwijderd

--- script/directive.js
+++
@@ -1,7 +0,0 @@
-var app = angular.module("bookApp");
-app.directive("testDirective", function() {
-    return {
-        restrict : "A",
-        template : "<h1>Made by a directive!</h1>"
-    };
-});
